1a68bac0000c1f4914fe8e82c79e317744a5cec3
Michael Cornelis <hello@cornelism.be>
2021-08-12 20:16:57 +0200

    cleanup and base html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="menu">
+        cornelism.be
+        <nav>
+          <a href="/" target="_self">
+            work
+          </a>
+          <a href="/blog" target="_self">
+            blog
+          </a>
+          <a href="/contact" target="_self">
+            contact
+          </a>
+        </nav>
+        <button>light</button>
+        <button>dark</button>
+      </div>
+      <div id="gallery">
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+        <div className="gallery-item">
+          <a href="http://www.google.be">
+            <img src="https://via.placeholder.com/800x600.png" alt="" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
