e9ed85ed6841f42bf787b6766b8fb738955a0c16
Michael Cornelis <hello@cornelism.be>
2022-03-30 09:00:32 +0200

    add Footer compo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,30 @@
 import React from "react";
-import Wrapper from "../Wrapper";
+import { Link, Wrapper } from "../";
 import styles from "./Footer.module.scss";
+import layoutStyles from "../Layout.module.scss";
 
 function Footer() {
   return (
     <Wrapper>
       <div className={styles.footer}>
-        <div>2022 - cornelism.be</div>
-        <div>PRIVACY POLICY</div>
+        <div>
+          cornelism.be
+          <br />
+          Melkerijstraat 34 bus 32
+          <br />
+          1500 Halle
+          <br />
+          Belgium
+        </div>
+
+        <div className={layoutStyles.alignRight}>
+          <Link block href="/terms-of-use">
+            terms of use
+          </Link>
+          <Link block href="/privacy-policy">
+            privacy policy
+          </Link>
+        </div>
       </div>
     </Wrapper>
   );
